d43eb736358521cd26ac756bb6e24771fb1c5620
Emmanuel Stone <emmanuel.stone@gmail.com>
2023-03-02 17:16:07 +0000

    Add form to CORS demo

--- cors/app.py
+++ cors/app.py
@@ -8,5 +8,20 @@ CORS(app)
 def ajax():
   return "This is the response from the remote server. The remote server is configured to allow cross origin requests"
 
+@app.route('/store-form', methods=['POST'])
+def store_form():
+	print("Storing form data...")
+	data = request.form
+	
+	# Store to database here
+	print("form data:", data)
+
+	return {
+		'emailId'	 : data['userEmail'],
+		'phoneNumber' : data['userContact'],
+		'password' : data['userPassword'],
+		'gender'	 : 'Male' if data['genderMale'] else 'Female',
+	}
+
 if __name__ == '__main__':
 	app.run(port=5050)
